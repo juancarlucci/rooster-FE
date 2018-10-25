f1e33a1d08b7ceb47edd7c9f9e0f7d8852a7ff98
juancarlucci <juancarloscollins@gmail.com>
2018-10-25 10:22:52 -0700

    new image resource path

--- src/Section1.js
+++ src/Section1.js
@@ -9,25 +9,25 @@ class Section1 extends Component {
       <article className="page-articles" id="section1">
         <section className="grid">
           <div className="page-item">
-            <img src="images/page-icon.svg" alt="page icon" />
+            <img src="./images/page-icon.svg" alt="page icon" />
             <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius soluta incidunt, eum quos</p>
           </div>
           <div className="page-item">
-            <img src="images/page-icon.svg" alt="page icon" />
+            <img src="./images/page-icon.svg" alt="page icon" />
             <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius soluta incidunt, eum quos</p>
           </div>
           <div className="page-item">
-            <img src="images/page-icon.svg" alt="page icon" />
+            <img src="./images/page-icon.svg" alt="page icon" />
             <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius soluta incidunt, eum quos</p>
           </div>
           <div className="page-item">
-            <img src="images/page-icon.svg" alt="page icon" />
+            <img src="./images/page-icon.svg" alt="page icon" />
             <p className="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius soluta incidunt, eum quos</p>
           </div>
         </section>
         <section className="slanted">
 
-          <img className="slanted-img" src="images/slanted-gradient-background.png" alt="slanted page" />
+          <img className="slanted-img" src="./images/slanted-gradient-background.png" alt="slanted page" />
 
           <p className="slanted-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam sit amet, consectetur adipisicing</p>
 
--- src/Section3.js
+++ src/Section3.js
@@ -17,7 +17,7 @@ class Section3 extends Component {
             <p className="header3">Lorem ipsum dolor sit amet lorem</p>
             <p className="lorem gray section3-lorem" id="">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
-          <img className="img1" src="/images/img1.jpg" alt="on the phone"/>
+          <img className="img1" src="./images/img1.jpg" alt="on the phone"/>
         </section>
         <section id="data" className="grid3">
           <div className="title-subtitle2">
@@ -25,7 +25,7 @@ class Section3 extends Component {
             <p className="header3">Lorem ipsum dolor sit amet lorem</p>
             <p className="lorem gray section3-lorem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
-          <img className="img2" src="/images/img2.jpg" alt="digital graph"/>
+          <img className="img2" src="./images/img2.jpg" alt="digital graph"/>
         </section>
       </article>
     );
