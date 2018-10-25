16d327070c43d7db8d15d885bb7f6187dac2c325
juancarlucci <juancarloscollins@gmail.com>
2018-10-24 21:25:52 -0700

    Updates

---
+++ precache-manifest.0acac96d1f1a0cb773d66dc5e1b88844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2277e277c3ea67e6ae2f",
+    "url": "/rooster-FE/static/js/runtime~main.2277e277.js"
+  },
+  {
+    "revision": "7eb71e8d43daa91ef237",
+    "url": "/rooster-FE/static/js/main.7eb71e8d.chunk.js"
+  },
+  {
+    "revision": "f34fc3cdc26786ae9881",
+    "url": "/rooster-FE/static/js/1.f34fc3cd.chunk.js"
+  },
+  {
+    "revision": "7eb71e8d43daa91ef237",
+    "url": "/rooster-FE/static/css/main.e76b8b11.chunk.css"
+  },
+  {
+    "revision": "f34fc3cdc26786ae9881",
+    "url": "/rooster-FE/static/css/1.ca228f0f.chunk.css"
+  },
+  {
+    "revision": "152616c4c3d0633bf207b6c09819eb3b",
+    "url": "/rooster-FE/index.html"
+  }
+];
