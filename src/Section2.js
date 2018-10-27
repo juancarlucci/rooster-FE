574f22423d10a5cd47d6236436c3b8b92dacbe4d
juancarlucci <juancarloscollins@gmail.com>
2018-10-27 15:31:07 -0700

    fix button width section2

--- src/Section2.js
+++ src/Section2.js
@@ -32,7 +32,7 @@ class Section2 extends Component {
         </section>
         <section className="grid2">
           <p className="page-item2 lorem gray" id="gray-ipsum-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <a href="#data"><button type="button">Button</button></a>
+            <a href="#data"><button id="percent-button" type="button">Button</button></a>
         </section>
         <ScrollAnimation animateIn="fadeIn">
         <section className="grid2" id="percents-section">
